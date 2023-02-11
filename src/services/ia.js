309f5da35b6564a908e342999b28cdead59bc9d3
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-11 16:46:58 -0300

    Add service IA with key

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,25 +1,35 @@
 import cohere from "cohere-ai";
 
-cohere.init('mJ9GVG9lcV8i07TJYOuQjqfcw4JB2y1CmirFXdX1');
+cohere.init('C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO');
 
-
+// fix function
 export async function fixMyEnglish(input){
   const response = await cohere.generate({
     model: 'xlarge',
     prompt: `This is a speel checker generator. 
   --
-    Incorrect sample: "I are good!"
-    Correct sample: "I am good!"
+  Incorrect sample: "I are good!"
+  Correct sample: "I am good!"
   --
-    Incorrect sample: "I have 22 years old."
-    Correct sample: "I am 22 years old."
+  Incorrect sample: "I have 22 years old."
+  Correct sample: "I am 22 years old."
   --
-    Incorrect sample: "I done't can know"
-    Correct sample: "I don't know"
+  Incorrect sample: "I done't can know"
+  Correct sample: "I don't know"
   --
-    Incorrect sample: "${input}"`,
+  Incorrect sample: "${input}"
+  Correct sample:`,
+      max_tokens: 40,
+      temperature: 0.3,
+      k: 0,
+      p: 1,
+      frequency_penalty: 0,
+      stop_sequences: ['--'],
+      return_likelihoods: 'NONE'
   })
+  return response.body.generations[0].text
 }
+
 
 
 
