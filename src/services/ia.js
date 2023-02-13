0ce22a79e33f19065fd6849f605b14e5089bf58a
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-13 06:08:15 -0300

    Add data xlarge

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,10 +1,30 @@
-import cohere from "cohere-ai";
+const COHERE_API_KEY = 'C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO'
+const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
 
-cohere.init('C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO');
+/* 
+curl --location --request POST 'https://api.cohere.ai/generate' \
+  --header 'Authorization: BEARER C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO' \
+  --header 'Content-Type: application/json' \
+  --header 'Cohere-Version: 2022-12-06' \
+  --data-raw '{
+      "model": "xlarge",
+      "prompt": "{prompt}",
+      "max_tokens": 100,
+      "temperature": 0.8,
+      "k": 1,
+      "p": 0.75,
+      "frequency_penalty": 0,
+      "presence_penalty": 0,
+      "stop_sequences": [],
+      "return_likelihoods": "NONE"
+    }'
+*/
+
+
 
 // fix function
 export async function fixMyEnglish(input){
-  const response = await cohere.generate({
+  const data = {
     model: 'xlarge',
     prompt: `This is a speel checker generator. 
   --
@@ -26,7 +46,17 @@ export async function fixMyEnglish(input){
       frequency_penalty: 0,
       stop_sequences: ['--'],
       return_likelihoods: 'NONE'
+  }
+  const response = fetch(COHERE_API_GENERATE_URL, {
+    method: 'POST',
+    headers: {
+      Authorization: `BEARER ${COHERE_API_KEY}`,
+      ContentType: 'application/json',
+      CohereVersion: '2022-12-06',
+      
+    }
   })
+  const response = await cohere.generate()
   return response.body.generations[0].text
 }
 
