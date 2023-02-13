738ad092ea3347621ce576bbf855b98801f0c584
leonvallejos <dev.leonvallejos@gmail.com>
2023-02-13 17:54:51 -0300

    Add url endpoint for detect language

--- src/services/ia.js
+++ src/services/ia.js
@@ -1,24 +1,24 @@
 const COHERE_API_KEY = 'C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO'
 const COHERE_API_GENERATE_URL = 'https://api.cohere.ai/generate'
+const COHERE_API_DETECT_LANGUAGE_URL = 'https://api.cohere.ai/detect-language'
 
-/* 
-curl --location --request POST 'https://api.cohere.ai/generate' \
-  --header 'Authorization: BEARER C1dVF3W8eKp2VJ1zCK2hEAk2CHSHJLKRfNmLpEXO' \
-  --header 'Content-Type: application/json' \
-  --header 'Cohere-Version: 2022-12-06' \
-  --data-raw '{
-      "model": "xlarge",
-      "prompt": "{prompt}",
-      "max_tokens": 100,
-      "temperature": 0.8,
-      "k": 1,
-      "p": 0.75,
-      "frequency_penalty": 0,
-      "presence_penalty": 0,
-      "stop_sequences": [],
-      "return_likelihoods": "NONE"
-    }'
-*/
+export async function checkIsEnglish(input){
+  const data = {
+    texts: [input],
+  }
+  const response = await fetch(COHERE_API_DETECT_LANGUAGE_URL, {
+    method: 'POST',
+    headers: {
+      Acccept: 'application/json',
+      Authorization: `BEARER ${COHERE_API_KEY}`,
+      "Content-Type": 'application/json',
+      "Cohere-Version": '2022-12-06',
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json())
+
+  const {language} = response
+}
 
 
 
@@ -47,17 +47,22 @@ export async function fixMyEnglish(input){
       stop_sequences: ['--'],
       return_likelihoods: 'NONE'
   }
-  const response = fetch(COHERE_API_GENERATE_URL, {
+  const response = await fetch(COHERE_API_GENERATE_URL, {
     method: 'POST',
     headers: {
       Authorization: `BEARER ${COHERE_API_KEY}`,
-      ContentType: 'application/json',
-      CohereVersion: '2022-12-06',
-      
-    }
-  })
-  const response = await cohere.generate()
-  return response.body.generations[0].text
+      "Content-Type": 'application/json',
+      "Cohere-Version": '2022-12-06',
+    },
+    body: JSON.stringify(data)
+  }).then(res => res.json())
+
+  const {text} = response.generations[0]
+
+  return text
+  .replace('--', '')
+  .replaceAll('"', '')
+  .substring(1, text.length - 3)
 }
 
 
